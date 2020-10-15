3400269d2c52c6ada3469d4a6f7d5e23a429072b
Oana Ujica <oana.ujica@fortech.ro>
2020-10-15 17:46:39 +0300

    add tsconfig.json

--- js/basic.js
+++ js/basic.js
@@ -1,22 +1,20 @@
-export class Customer {
-    name: string;
-    isActive: boolean;
-
-    constructor(name: string) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Customer = void 0;
+class Customer {
+    constructor(name) {
         this.name = name;
         this.isActive = true;
     }
-
     announce() {
         return 'Hello, my name is ' + this.name;
     }
 }
-
+exports.Customer = Customer;
 //create a new instance
 let firstCustomer = new Customer('Oana');
-let newMessage: string = firstCustomer.announce();
-
+let newMessage = firstCustomer.announce();
 //change the text on the page
 //take the reference
 let webHeading = document.querySelector('h1');
-webHeading!.textContent = newMessage;
+webHeading.textContent = newMessage;
---
+++ js/other.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const basic_1 = require("./basic");
+// now, use the Customer class
+let myCustomer = new basic_1.Customer('Remus');
+console.log(myCustomer.announce());
